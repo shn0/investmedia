f0ffd20d8b3b0ea5cfd440b0ce3c1077ffd7d11c
Shadesh <narainshadesh@gmail.com>
2025-01-04 16:38:33 +0700

    tested post for user registration

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const User = require('../models/User'); //import user schema
 const router = express.Router();
 
 
-router.get('/test', (req, res) => {
-  res.send('User route is working!');
+
+router.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
+
+try {
+  // check if the user already exists 
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({ message: 'User already exists' });
+  }
+
+  // create a new user 
+  const newUser = new User({ name, email, password});
+  await newUser.save();
+
+  res.status(201).json({ message: 'User registered successfully!' });
+} catch (error) {
+  res.status(500).json({ message: 'Server error', error});
+
+}
+
+console.error('Error during registration:', error);
+res.status(500).json({ message: 'Server error', error });
+
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,23 @@
-require('dotenv').config();
-
+const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected!'))
 .catch((err) => console.error('MongoDB connection error:', err));
+
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
