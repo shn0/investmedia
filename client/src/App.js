836ea30855c090cf089519ddeaa6008967f470e8
Shadesh <narainshadesh@gmail.com>
2025-01-04 15:42:10 +0700

    imported navbar.js to main app

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Feed from './pages/feed';
 import PostForm from './pages/postform';
 import Profile from './pages/profile';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
+       <Navbar />
       <Routes>
         <Route path="/" element={<Feed />} />
         <Route path="/post" element={<PostForm />} />
